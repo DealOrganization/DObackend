2c7267569f2053ccbbdddf07df6fa1cde0c8526f
Kang1221 <90603399+Kang1221@users.noreply.github.com>
2023-01-24 14:45:17 +0900

    extract color using extcolors module

--- Yeonsoo/extract_color.py
+++ Yeonsoo/extract_color.py
@@ -1,11 +1,12 @@
 import extcolors
-from PIL import Image
+#from PIL import Image
 
-colors, pixel_count = extcolors.extract_from_path("carrot.jpg")
-print(colors)
 
-pixel_output = 0
-for c in colors:
-    pixel_output += c[1]
-    print(f'{c[0]} : {round((c[1] / pixel_count) * 100, 2)}% ({c[1]})')
-print(f'Pixels in output: {pixel_output} of {pixel_count}')
+
+for i in range (1,5):
+    img = "./images/carrot0"
+    img = img + str(i)+ ".jpg"
+    colors, pixel_count = extcolors.extract_from_path(img)
+    print(colors)
+
+
