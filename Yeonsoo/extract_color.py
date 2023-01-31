5215ff55e10dbeec0998f14f56562942b9edb0c4
Kang1221 <90603399+Kang1221@users.noreply.github.com>
2023-01-31 18:30:55 +0900

    add results of extracting colors for test images

--- Yeonsoo/extract_color.py
+++ Yeonsoo/extract_color.py
@@ -4,9 +4,11 @@ import extcolors
 
 
 for i in range (1,5):
-    img = "./images/carrot0"
+    img = "./images/lightning0"
     img = img + str(i)+ ".jpg"
     colors, pixel_count = extcolors.extract_from_path(img)
+    print(i)
     print(colors)
+    print()
 
 
